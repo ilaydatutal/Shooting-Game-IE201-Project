fbd73b2541546881ba7a567969c2201266e2cb1b
İlayda Tutal <tutalilayda@gmail.com>
2020-04-27 16:50:23 +0300

    added Object class

--- Object.cpp
+++ Object.cpp
@@ -1,1 +1,11 @@
 #include "Object.h"
+Object::Object() {
+
+}
+
+void Object::move(float elapsedTime, double directionX = NULL, double directionY = NULL, Hero hero = NULL) {
+
+    this->objX += this->dirX * this->speed * elapsedTime;
+    this->objY += this->dirY * this->speed * elapsedTime;
+}
+
--- Object.h
+++ Object.h
@@ -1,5 +1,19 @@
 #pragma once
+#include "olcSimpleEngine.h"
+using namespace std;
+
 class Object
 {
+	double objX;
+	double objY;
+	double dirX;
+	double dirY;
+	double speed;
+	static shared_ptr<olc::Sprite> sprite;
+
+public:
+	Object();
+	void move(float elapsedTime, double directionX = NULL, double directionY = NULL, Hero hero = NULL);
+	void draw();
 };
 
